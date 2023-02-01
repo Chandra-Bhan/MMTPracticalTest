4a0d68ac8b6b66169048fd889779ce6608c7214a
Chandra-Bhan <121319182+Chandra-Bhan@users.noreply.github.com>
2023-02-01 15:31:57 +0530

    Update HomeController.cs
    updating the comments

--- MMTPracticalTest/Controllers/HomeController.cs
+++ MMTPracticalTest/Controllers/HomeController.cs
@@ -8,6 +8,16 @@ using System.Web;
 using System.Web.Mvc;
 using MMTPracticalTest.Models;
 
+
+
+// Dear Sir/Mam This project not a completed project  because when I bind the state and city dropdown
+//   then i got some exceptions. State and city dropdown data make some issue when i fetch the data from the
+//    dropdowns this exception take so much time and yet not fixed. 
+
+//      In this Project only the user interface that is show with modal popup.
+
+
+
 namespace MMTPracticalTest.Controllers
 {
     public class HomeController : Controller
